d0d21e5807aab0b2d09cc736ff0688ece9ef80da
prudhvi144 <prudhvi9@bu.edu>
2022-07-27 17:45:24 -0400

    Created a folder Structure for the Projects using cookiecutter
    ref:https://medium.com/analytics-vidhya/folder-structure-for-machine-learning-projects-a7e451a8caaa

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identify potential sources of bias - By training deep learning models for analyzing images of human embryos.',
+    author='Prudhvi_Shafieelab',
+    license='MIT',
+)
